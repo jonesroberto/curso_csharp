0ba9e7e1b10aafb4b45a879e451de6025de10082
jones <jones@DESKTOP-LDHUHUF>
2019-12-12 23:38:29 -0300

    variáveis

--- src/Aula1/Program.cs
+++ src/Aula1/Program.cs
@@ -4,6 +4,12 @@ namespace Aula1
 {
     class Program
     {
+        int i; // variável não inicializada
+        int j = 99; // variavel inicializada
+        float saldo, valor, taxa; // variaveis do mesmo tipo separadas por ","
+        decimal desconto = 900, iof; // variaveis do mesmo tipo apenas uma inicializada
+        int total = 10 + 20; // variavel inicializada com o valor de uma expressão
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
