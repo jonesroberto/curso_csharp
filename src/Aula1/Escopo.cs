7b798a8c28759419296724a2094a7e96fcb0ba59
jones <jones@DESKTOP-LDHUHUF>
2019-12-12 23:48:01 -0300

    add class escopo

---
+++ src/Aula1/Escopo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula1
+{
+    public class Escopo
+    {
+        void Execute()
+        {
+            int varA; // no escopo do metodo Execute()
+            varA = 20;
+
+            Console.Clear();
+
+            if (varA == 20)
+            { 
+                int varB = 30; // somente no escopo do "if"
+                Console.WriteLine($"varA :{varA}"); //20
+                Console.WriteLine($"varB: {varB}"); //30
+            }
+
+            //varB = 40; //daria erro por estar fora do escopo
+        }
+    }
+}
