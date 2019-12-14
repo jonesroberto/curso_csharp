83d6d05ca31b165adb14b877f2b31e37db024463
jones <jones@DESKTOP-LDHUHUF>
2019-12-14 14:04:39 -0300

    conversões Aula 2

---
+++ src/Aula2/ConversoesTipos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula2
+{
+    public static class ConversoesTipos
+    {
+        public static void Execute()
+        {
+            ExecuteImplicita();
+            ExecuteExplicita();
+        }
+
+        public static void ExecuteImplicita()
+        {
+            int myInt = 98;
+            long myLong = myInt;
+
+            Console.WriteLine($"Valor long: {myLong}");
+        }
+
+        public static void ExecuteExplicita()
+        {
+            int myInt = 98;
+            long myLong = (long)myInt;
+
+            Console.WriteLine($"Valor long: {myLong}");
+        }
+    }
+}
--- src/Aula2/Program.cs
+++ src/Aula2/Program.cs
@@ -6,8 +6,38 @@ namespace Aula2
     {
         static void Main(string[] args)
         {
-            TipoValorPredefinidos.Execute();
-            TipoValorDefinidosUsuarios.Execute();
+            Console.WriteLine("Digite um valor para executar o Programa");
+            Console.WriteLine("1 - TipoValorPredefinidos");
+            Console.WriteLine("2 - TipoValorDefinidosUsuarios");
+            Console.WriteLine("3 - ConversoesTipos");
+
+            var command = Console.ReadKey();
+            Console.WriteLine("");
+
+            switch (command.Key)
+            {
+                case ConsoleKey.NumPad1:
+                TipoValorPredefinidos.Execute();
+                    break;
+                case ConsoleKey.D1:
+                    TipoValorPredefinidos.Execute();
+                    break;
+                case ConsoleKey.NumPad2:
+                    TipoValorDefinidosUsuarios.Execute();
+                    break;
+                case ConsoleKey.D2:
+                    TipoValorDefinidosUsuarios.Execute();
+                    break;
+                case ConsoleKey.NumPad3:
+                    ConversoesTipos.Execute();
+                    break;
+                case ConsoleKey.D3:
+                    ConversoesTipos.Execute();
+                    break;
+                default:
+                    Console.WriteLine(" Digite uma tecla válida!");
+                    break;    
+            }
 
             Console.WriteLine("Please any key to close application!");
             Console.ReadKey();
