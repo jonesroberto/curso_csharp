77149c25a0b72ce8d9364cf1b207c73df89ed9ab
Jones Roberto da Silva <jones.roberto@xpi.com.br>
2019-12-15 21:14:07 -0300

    add foreach

---
+++ src/Aula5/Aula5_Iteracao/ForEachs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula5_Iteracao
+{
+    public static class ForEachs
+    {
+        public static void Execute()
+        {
+            ExecuteForEach();
+        }
+
+        static void ExecuteForEach()
+        {
+            int[] pares = new int[5];
+
+            for (int i = 0; i < 5; ++i)
+            {
+                pares[i] = (i * 2) + 2;
+            }
+
+            foreach (var item in pares)
+            {
+                Console.WriteLine($"Número : {item}");
+            }
+        }
+    }
+}
